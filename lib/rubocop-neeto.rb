763455692997bfbe34e20523fa77393cbf0d4830
Amaljith K <amaljith.k@bigbinary.com>
2023-08-16 10:38:30 +0530

    Initial commit

---
+++ lib/rubocop-neeto.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+require_relative 'rubocop/neeto'
+require_relative 'rubocop/neeto/version'
+require_relative 'rubocop/neeto/inject'
+
+RuboCop::Neeto::Inject.defaults!
+
+require_relative 'rubocop/cop/neeto_cops'
